e92f7ef7ae09a16dfdbad8391a85c6df140ce8de
leonevil <leongb66@gmail.com>
2020-07-30 22:02:49 -0300

    Resolucao URI 1072

---
+++ JAVA/URI-1072/src/application/Main.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		int in = 0;
+		int out = 0;
+		
+		for (int i = 0; i < x; i++) {
+			int y = sc.nextInt();
+			
+			if (y >= 10 && y <= 20) {
+				in++;
+			} else {
+				out++;
+			}
+		}
+		
+		System.out.println(in + " in");
+		System.out.println(out + " out");
+		
+		sc.close();
+	}
+}
