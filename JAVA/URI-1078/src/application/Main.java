5a27f03c0d009c1b490987a3cbbcf71fc73f88ad
leonevil <leongb66@gmail.com>
2020-07-31 21:19:13 -0300

    Resolucao URI 1078

---
+++ JAVA/URI-1078/src/application/Main.java
@@ -0,0 +1,20 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i + " x " + x + " = " + (i * x));
+		}
+		
+		sc.close();
+	}
+}
